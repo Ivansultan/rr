2ebeaec02796d9fc5111ee2a6a456e08c2063301
Artem Serga <artem@serga.name>
2020-09-10 20:46:29 +0300

    Updates

---
+++ precache-manifest.ca563b679d62932749ee929730d3a04b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e17a0f18af8e3277d06d24a360acd3e",
+    "url": "/rr/index.html"
+  },
+  {
+    "revision": "576d1f62dbe92c882969",
+    "url": "/rr/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "327fab0a629eb73f9bdf",
+    "url": "/rr/static/js/2.fd116a51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rr/static/js/2.fd116a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576d1f62dbe92c882969",
+    "url": "/rr/static/js/main.333c6b56.chunk.js"
+  },
+  {
+    "revision": "3eb9276817f6a062030a",
+    "url": "/rr/static/js/runtime-main.10ff1773.js"
+  }
+]);
