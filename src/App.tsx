645f67c98335f61d52b1538f750e252c2041c300
Ivan Soltynchuk <isoltynchuk@gmail.com>
2020-09-15 12:56:22 +0300

    Rename photo, new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+import logo from "./unnamed.jpg"; // Tell webpack this JS file uses this image
+import avatar from "./unnamed.png";
 
 // This site has 3 pages, all of which are rendered
 // dynamically in the browser (not server rendered).
@@ -21,6 +30,9 @@ export default function BasicExample() {
           <li>
             <Link to="/about">About</Link>
           </li>
+          <li>
+            <Link to="/contacts">Contacts</Link>
+          </li>
         </ul>
 
         <hr />
@@ -34,10 +46,14 @@ export default function BasicExample() {
         */}
         <Switch>
           <Route exact path="/">
-            <Home />
+            <HomeList />
           </Route>
+
           <Route path="/about">
             <About />
+          </Route>
+          <Route path="/contacts">
+            <Contacts />
           </Route>
         </Switch>
       </div>
@@ -48,10 +64,39 @@ export default function BasicExample() {
 // You can think of these components as "pages"
 // in your app.
 
-function Home() {
+function HomeList() {
+  let { path, url } = useRouteMatch();
   return (
     <div>
-      <h2>Home</h2>
+      <h1>Home</h1>
+      <ul>
+        <li>
+          <Link to={`${url}/vietnamese`}>Vietnamese massage </Link>
+        </li>
+        <li>
+          <Link to={`${url}/relaxing`}>Relaxing massage</Link>
+        </li>
+        <li>
+          <Link to={`${url}/relaxing`}>Belly massage</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path={path}></Route>
+        <Route path={`${path}/:homeId`}>
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Home() {
+  let homeId = useParams();
+
+  return (
+    <div>
+      <h3>{homeId}</h3>
     </div>
   );
 }
@@ -59,7 +104,32 @@ function Home() {
 function About() {
   return (
     <div>
-      <h2>About</h2>
+      <h1>About</h1>
+      <img src={logo} alt="Logo" />
+      <ul>
+        Солтынчук Евгений Леонидович 07.04.1982 Образование
+        высшее,педагогическое. Работал в американских компаниях “Cunard”,
+        “Princess” в течении 11 лет, в качестве профессионального танцора.
+        Благодаря 20-ти летнему танцевальному стажу, есть опыт техники
+        восстановления тела после травм, переломов, растяжений, физической и
+        эмоциональной перегрузке , а также по биомеханике тела и работы мышц.
+      </ul>
+    </div>
+  );
+}
+
+function Contacts() {
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <img src={avatar} alt="Avatar" />
+      <ul>
+        Все вышеперечисленные виды массажа, хорошо работают, как для мужчин ,
+        так и для женщин. И в зависимости от состояния здоровья и физической
+        кондиции человека, могут исполняться с легкой, средней и интенсивной
+        нагрузкой. Солтынчук Евгений Леонидович bigmavr5@gmail.com +38 093 110
+        44 35 +38 066 118 16 08
+      </ul>
     </div>
   );
 }
