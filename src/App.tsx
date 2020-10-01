dc7b53f249ec5cf3ae6890e803b7ec916a9dc2d2
Ivan Soltynchuk <isoltynchuk@gmail.com>
2020-10-01 20:14:19 +0300

    File About

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Timeline from "@material-ui/lab/Timeline";
-import TimelineItem from "@material-ui/lab/TimelineItem";
-import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
-import TimelineConnector from "@material-ui/lab/TimelineConnector";
-import TimelineContent from "@material-ui/lab/TimelineContent";
-import TimelineOppositeContent from "@material-ui/lab/TimelineOppositeContent";
-import TimelineDot from "@material-ui/lab/TimelineDot";
-import FastfoodIcon from "@material-ui/icons/Fastfood";
-import LaptopMacIcon from "@material-ui/icons/LaptopMac";
-import HotelIcon from "@material-ui/icons/Hotel";
-import RepeatIcon from "@material-ui/icons/Repeat";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import clsx from "clsx";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
-import { red } from "@material-ui/core/colors";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import ShareIcon from "@material-ui/icons/Share";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-
+import { About } from "./components/About/About";
 import {
   BrowserRouter as Router,
   Switch,
@@ -86,11 +59,7 @@ export default function App() {
             width: "100%",
             marginTop: "15px",
           }}
-        >
-          {oldLinks.map((link) => (
-            <OldSchoolMenuLink to={link.to} label={link.label} />
-          ))}
-        </h1>
+        ></h1>
 
         <hr />
 
@@ -109,12 +78,14 @@ export default function App() {
             <MassagesRoute />
           </Route>
 
+          {/*
           <Route path="/about">
-            <About />
+            <AboutPage />
           </Route>
           <Route path="/contacts">
             <Contacts />
           </Route>
+          */}
         </Switch>
       </div>
     </Router>
@@ -174,122 +145,6 @@ function MassagesRoute() {
       </div>
 
       <Massages />
-
-      {/*  <Card style={{ backgroundColor: "gray" }}>
-        <div>
-          {Object.values(massagesData.japanese).map((value) => {
-            return <div>{value}</div>;
-          })}
-        </div>
-      </Card>
-
-      <Card style={{ backgroundColor: "white" }}>
-        <div>
-          {Object.values(massagesData.vietnamese).map((value) => {
-            return <div>{value}</div>;
-          })}
-        </div>
-      </Card>
-
-      <Card style={{ backgroundColor: "gray" }}>
-        <div>
-          {Object.values(massagesData.relaxing).map((value) => {
-            return <div>{value}</div>;
-          })}
-        </div>
-      </Card>
-
-      <Card style={{ backgroundColor: "white" }}>
-        <div>
-          {Object.values(massagesData.visceral).map((value) => {
-            return <div>{value}</div>;
-          })}
-        </div>
-      </Card> 
-        */}
-
-      <Timeline align="alternate">
-        <TimelineItem style={{ marginTop: "150px" }}>
-          <TimelineOppositeContent>
-            <Typography variant="body2" color="textSecondary">
-              2002-2007
-            </Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot>
-              <FastfoodIcon />
-            </TimelineDot>
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Paper elevation={3} className={classes.paper}>
-              <Typography variant="h6" component="h1">
-                Образование
-              </Typography>
-              <Typography>Высшее педагогическое (бла,бла,бла...)</Typography>
-            </Paper>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            <Typography variant="body2" color="textSecondary">
-              2008 - 2019
-            </Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot color="primary">
-              <LaptopMacIcon />
-            </TimelineDot>
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Paper elevation={3} className={classes.paper}>
-              <Typography variant="h6" component="h1">
-                Работа на кораблях
-              </Typography>
-              <Typography>Хочу ещё разок:)</Typography>
-            </Paper>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent>
-            <Typography variant="body2" color="textSecondary">
-              2015 - 2020
-            </Typography>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot color="primary" variant="outlined">
-              <HotelIcon />
-            </TimelineDot>
-            <TimelineConnector className={classes.secondaryTail} />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Paper elevation={3} className={classes.paper}>
-              <Typography variant="h6" component="h1">
-                Стаж работы массажистом
-              </Typography>
-              <Typography>5 лет, но можно написать сколько хочу...</Typography>
-            </Paper>
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot color="secondary">
-              <RepeatIcon />
-            </TimelineDot>
-          </TimelineSeparator>
-          <TimelineContent>
-            <Paper elevation={3} className={classes.paper}>
-              <Typography variant="h6" component="h1">
-                Доп. инфа
-              </Typography>
-              <Typography>
-                Очень нерешительльный самоубийца, умер от старости
-              </Typography>
-            </Paper>
-          </TimelineContent>
-        </TimelineItem>
-      </Timeline>
     </Router>
   );
 }
@@ -336,28 +191,24 @@ function NewSchoolMenuLink({ label, to, activeOnlyWhenExact }: any) {
   );
 }
 
-function MassagesPage() {
-  let massageId = (useParams() as any).massageId as MassageType;
-  console.log("useParams", useParams());
+// function MassagesPage() {
+//   let massageId = (useParams() as any).massageId as MassageType;
+//   console.log("useParams", useParams());
 
-  const { title, description, images } = massagesData[massageId];
+//   const { title, description, images } = massagesData[massageId];
 
-  return (
-    <div>
-      <h3>{title}</h3>
+//   return (
+//     <div>
+//       <h3>{title}</h3>
 
-      {images.map((image: string) => {
-        return <img src={image}>{}</img>;
-      })}
-      {/* <img src={image} alt="Фото" /> */}
-      <div>{description}</div>
-    </div>
-  );
-}
-
-function About() {
-  return <div></div>;
-}
+//       {images.map((image: string) => {
+//         return <img src={image}>{}</img>;
+//       })}
+//       {/* <img src={image} alt="Фото" /> */}
+//       <div>{description}</div>
+//     </div>
+//   );
+// }
 
 //     <div>
 //       <h1>About</h1>
@@ -380,21 +231,25 @@ function About() {
 //   );
 // }
 
-function Contacts() {
-  return (
-    <div>
-      <img
-        style={{ maxWidth: "30%", maxHeight: "30%" }}
-        src={avatar}
-        alt="Avatar"
-      />
-      <ul>
-        Все вышеперечисленные виды массажа, хорошо работают, как для мужчин ,
-        так и для женщин. И в зависимости от состояния здоровья и физической
-        кондиции человека, могут исполняться с легкой, средней и интенсивной
-        нагрузкой. Солтынчук Евгений Леонидович bigmavr5@gmail.com +38 093 110
-        44 35 +38 066 118 16 08
-      </ul>
-    </div>
-  );
-}
+// function Contacts(props) {
+//   return (
+//     <div>
+//       <img
+//         style={{ maxWidth: "30%", maxHeight: "30%" }}
+//         src={avatar}
+//         alt="Avatar"
+//       />
+//       <ul>
+//         Все вышеперечисленные виды массажа, хорошо работают, как для мужчин ,
+//         так и для женщин. И в зависимости от состояния здоровья и физической
+//         кондиции человека, могут исполняться с легкой, средней и интенсивной
+//         нагрузкой. Солтынчук Евгений Леонидович bigmavr5@gmail.com +38 093 110
+//         44 35 +38 066 118 16 08
+//       </ul>
+//     </div>
+//   );
+// }
+
+// function AboutPage(props) {
+//   return <div>About</div>;
+// }
---
+++ src/components/About/About.tsx
@@ -0,0 +1,117 @@
+import React, { Props } from "react";
+import Timeline from "@material-ui/lab/Timeline";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineOppositeContent from "@material-ui/lab/TimelineOppositeContent";
+import { Typography } from "@material-ui/core";
+import TimelineDot from "@material-ui/lab/TimelineDot";
+import classes from "*.module.css";
+import { Router } from "react-router-dom";
+import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
+import TimelineConnector from "@material-ui/lab/TimelineConnector";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import FastfoodIcon from "@material-ui/icons/Fastfood";
+import LaptopMacIcon from "@material-ui/icons/LaptopMac";
+import HotelIcon from "@material-ui/icons/Hotel";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import Paper from "@material-ui/core/Paper";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+export const About = () => {
+  return (
+    <Timeline align="alternate">
+      <TimelineItem style={{ marginTop: "150px" }}>
+        <TimelineOppositeContent>
+          <Typography variant="body2" color="textSecondary">
+            2002-2007
+          </Typography>
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot>
+            <FastfoodIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h6" component="h1">
+              Образование
+            </Typography>
+            <Typography>Высшее педагогическое (бла,бла,бла...)</Typography>
+          </Paper>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent>
+          <Typography variant="body2" color="textSecondary">
+            2008 - 2019
+          </Typography>
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot color="primary">
+            <LaptopMacIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h6" component="h1">
+              Работа на кораблях
+            </Typography>
+            <Typography>Хочу ещё разок:)</Typography>
+          </Paper>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineOppositeContent>
+          <Typography variant="body2" color="textSecondary">
+            2015 - 2020
+          </Typography>
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineDot color="primary" variant="outlined">
+            <HotelIcon />
+          </TimelineDot>
+          <TimelineConnector className={classes.secondaryTail} />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h6" component="h1">
+              Стаж работы массажистом
+            </Typography>
+            <Typography>5 лет, но можно написать сколько хочу...</Typography>
+          </Paper>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot color="secondary">
+            <RepeatIcon />
+          </TimelineDot>
+        </TimelineSeparator>
+        <TimelineContent>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h6" component="h1">
+              Доп. инфа
+            </Typography>
+            <Typography>
+              Очень нерешительльный самоубийца, умер от старости
+            </Typography>
+          </Paper>
+        </TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
+};
