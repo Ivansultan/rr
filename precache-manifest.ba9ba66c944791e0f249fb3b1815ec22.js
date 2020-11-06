f930c2c3984ae4df854ad33eb1e241f85b4af98d
Ivan Soltynchuk <isoltynchuk@gmail.com>
2020-11-06 14:50:05 +0200

    Updates

---
+++ precache-manifest.ba9ba66c944791e0f249fb3b1815ec22.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8419ede7c72cf9c77fe3af591af6a1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38632e26c8f6f4517812",
+    "url": "/static/css/main.8f41e826.chunk.css"
+  },
+  {
+    "revision": "76d43e54afd8b8cf0050",
+    "url": "/static/js/2.3b4fc8ca.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.3b4fc8ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38632e26c8f6f4517812",
+    "url": "/static/js/main.c5f5603c.chunk.js"
+  },
+  {
+    "revision": "461d1ec57b23cc872dbd",
+    "url": "/static/js/runtime-main.26944662.js"
+  },
+  {
+    "revision": "809234ff73550348bd5b4a3e7ddb3a98",
+    "url": "/static/media/contacts.809234ff.png"
+  },
+  {
+    "revision": "ec9769b1afe293c99b22968c2bf7e917",
+    "url": "/static/media/diploma(1).ec9769b1.jpg"
+  },
+  {
+    "revision": "2ac2fe23ce95f7848d6672e583708d2e",
+    "url": "/static/media/diploma(2).2ac2fe23.jpg"
+  },
+  {
+    "revision": "b65a5907839ec7ce83de16feb94702d2",
+    "url": "/static/media/diploma(3).b65a5907.jpg"
+  },
+  {
+    "revision": "f217ab3a03105478b3f9b698765bc694",
+    "url": "/static/media/diploma(4).f217ab3a.jpg"
+  },
+  {
+    "revision": "f8fa5ad011e56770c7c7e9374bb5c578",
+    "url": "/static/media/japanese1.f8fa5ad0.jpg"
+  },
+  {
+    "revision": "a0f148521714935c9ef463cf6b0b852a",
+    "url": "/static/media/japanese2.a0f14852.jpg"
+  },
+  {
+    "revision": "beb6017fcd06a8ea97c701d53c54041c",
+    "url": "/static/media/japanese3.beb6017f.jpg"
+  },
+  {
+    "revision": "c423c09451cfaa9513e1e826e8ebe3fb",
+    "url": "/static/media/japanese4.c423c094.jpg"
+  },
+  {
+    "revision": "29cc1f6009496473abd4721eb40af9bc",
+    "url": "/static/media/jensHand.29cc1f60.png"
+  },
+  {
+    "revision": "30a6649617317db4cc4b26ac1dd7deae",
+    "url": "/static/media/relaxing(1).30a66496.jpg"
+  },
+  {
+    "revision": "7d4133b8d68d1f63408eafb535c67337",
+    "url": "/static/media/relaxing(2).7d4133b8.jpg"
+  },
+  {
+    "revision": "aada6d7a222fc7d195e635010a3392b7",
+    "url": "/static/media/relaxing(3).aada6d7a.jpg"
+  },
+  {
+    "revision": "9d2b3fd5e1c25be6a1a654fb5122d2e4",
+    "url": "/static/media/relaxing(4).9d2b3fd5.jpg"
+  },
+  {
+    "revision": "615defec8cea19a8f770a508936f19eb",
+    "url": "/static/media/vietnamese1.615defec.jpg"
+  },
+  {
+    "revision": "d62abe8695ee1a440f0d12abcd94fecf",
+    "url": "/static/media/vietnamese2.d62abe86.jpg"
+  },
+  {
+    "revision": "975a7378b9f1346bdc02f09cb941921c",
+    "url": "/static/media/vietnamese3.975a7378.jpg"
+  },
+  {
+    "revision": "b1527291175dfa572cae69d8daf624eb",
+    "url": "/static/media/vietnamese4.b1527291.jpg"
+  },
+  {
+    "revision": "e2338929b65cfaec7b8a61ace01130fe",
+    "url": "/static/media/visceral(1).e2338929.jpg"
+  },
+  {
+    "revision": "ab77649cc4697b5d244dc00898fad4e8",
+    "url": "/static/media/visceral(2).ab77649c.jpg"
+  },
+  {
+    "revision": "dcf54cc07861829b437badabbb848281",
+    "url": "/static/media/visceral(3).dcf54cc0.jpg"
+  },
+  {
+    "revision": "a795e2485de977bc6c1724de6cd82da9",
+    "url": "/static/media/visceral(4).a795e248.jpg"
+  }
+]);
