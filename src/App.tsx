33742e2636a1e3451849028bd031f453d27dfc7d
Ivan Soltynchuk <isoltynchuk@gmail.com>
2020-11-11 20:34:08 +0200

    Merge changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { About } from "./components/About/About";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import { Massages } from "./components/Massages/Massages";
 import Switch from "@material-ui/core/Switch";
@@ -12,122 +11,130 @@ import Russian from "./translations/ru.json";
 import English from "./translations/en.json";
 import Contacts from "./components/Contacts";
 
+type Locale = "en" | "ru";
+
+type State = {
+  locale: Locale;
+};
+
+type Action = State & {
+  type: "setLocale";
+};
 
 const messages = {
   en: English,
   ru: Russian,
 };
 
-const locale = "en";
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "setLocale":
+      return { locale: action.locale };
+    default:
+      throw new Error();
+  }
+};
 
+type Language = {
+  name: string;
+  locale: Locale;
+};
 
+const languages: Language[] = [
+  { name: "English", locale: "en" },
+  { name: "Russian", locale: "ru" },
+];
 
 export default function App() {
-  const handleChange = (event: any) => {
-    localStorage.setItem("locale", event.target.checked ? "en" : "ru");
-    setState({ ...state, [event.target.name]: event.target.checked });
-  };
+  const [state, dispatch] = useReducer(reducer, { locale: "en" });
 
-  const [state, setState] = React.useState({
-    checkedA: true,
-  });
-
+  const { locale } = state;
   return (
     <IntlProvider locale={locale} messages={messages[locale]}>
-      <Router>
-        <div>
+      <div>
+        <div
+          style={{
+            // backgroundColor: "gray",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
           <div
             style={{
-             // backgroundColor: "gray",
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-around",
-              alignItems: "center",
+              // backgroundColor: "red",
+              marginLeft: 25,
+            }}
+            onClick={() => {
+              alert("Redirect to home page");
             }}
           >
-            <div
-              style={{
-               // backgroundColor: "red",
-                marginLeft: 25,
-              }}
-              onClick={() => {
-                alert("Redirect to home page");
-              }}
-            >
-              <img
-                alt="jestHand"
-                src={require("./components/Massages/media/jensHand.png")}
-                width="60"
-              />
-            </div>
-            <div
-              style={{
-               // backgroundColor: "green",
-                flex: 1,
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                display: "flex",
-                marginRight: 25,
-              }}
-            >
-              
-                <h1 style={{ textAlign: "center", marginTop: 7, marginBottom: 7 }}>
-                <FormattedMessage id="Massage to relax and rehab"/>
-                </h1>
-              
+            <img
+              alt="jestHand"
+              src={require("./components/Massages/media/jensHand.png")}
+              width="60"
+            />
+          </div>
+          <div
+            style={{
+              // backgroundColor: "green",
+              flex: 1,
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              display: "flex",
+              marginRight: 25,
+            }}
+          >
+            <h1 style={{ textAlign: "center", marginTop: 7, marginBottom: 7 }}>
+              <FormattedMessage id="Massage to relax and rehab" />
+            </h1>
 
-              <div>
-                <FormGroup>
-                  <Typography component="div">
-                    <Grid
-                      component="label"
-                      container
-                      alignItems="center"
-                      spacing={1}
-                    >
-                      <Grid item>RUS</Grid>
-                      <Grid item>
-                        <Switch
-                          checked={state.checkedA}
-                          onChange={handleChange}
-                          name="checkedA"
-                        />
-                      </Grid>
-                      <Grid item>ENG</Grid>
-                    </Grid>
-                  </Typography>
-                </FormGroup>
-                
-              </div>
+            <div>
+              <FormGroup>
+                <Typography component="div">
+                  <Grid
+                    component="label"
+                    container
+                    alignItems="center"
+                    spacing={1}
+                  >
+                    {languages.map((language) => {
+                      const isActiveLocale = language.locale === state.locale
+                      return (
+                        <div
+                          style={{
+                            paddingLeft: 10,
+                            cursor: isActiveLocale
+                                ? "default"
+                                : "pointer",
+                            fontWeight: isActiveLocale ? "bold" : "normal"
+                          }}
+                          onClick={() => {
+                            if (!isActiveLocale) {
+                              dispatch({
+                                type: "setLocale",
+                                locale: language.locale,
+                              })
+                            }
+                          }}
+                        >
+                          {language.name}
+                        </div>
+                      );
+                    })}
+                  </Grid>
+                </Typography>
+              </FormGroup>
             </div>
           </div>
-          <hr style={{marginBottom: 30, border: "1px dotted #f0f5f5"}}/>
-
-          <Redirect to="/massages" />
-
-          <Route path="/massages">
-            <MassagesRoute />
-          </Route>
-
-          {/* <Route path="/contacts">
-            <Contacts />
-          </Route> */}
         </div>
-      </Router>
+        <hr style={{ marginBottom: 30, border: "1px dotted #f0f5f5" }} />
+      </div>
+      <Massages />
+      <About />
+      <Contacts />
     </IntlProvider>
   );
 }
-
-function MassagesRoute() {
-  return (
-    <Router>
-      <Massages />
-
-      <About />
-
-      <Contacts />
-    </Router>
-  );
-}
-
