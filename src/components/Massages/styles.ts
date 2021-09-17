f158616c8b77c16667cb058be5991830b2963da6
Ivan Soltynchuk <isoltynchuk@gmail.com>
2021-09-17 14:20:20 +0300

    Add favicon

--- src/components/Massages/styles.ts
+++ src/components/Massages/styles.ts
@@ -32,7 +32,8 @@ export const useStyles = makeStyles((theme) => ({
       backgroundColor: "transparent",
     },
     paper: {
-      backgroundColor: theme.palette.background.paper,
+      //backgroundColor: theme.palette.background.paper,
+      backgroundColor: "#ebfafa",
       border: "2px solid #000",
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3),
